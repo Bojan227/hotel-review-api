63b5b670b0a8f310fe2ae9443651207656c80ee9
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-23 23:18:12 +0100

    Initial installatin of express and connecting to MongoDB

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// require dotenv file
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// import routes
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/user', userRoutes);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  app.listen(3000, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+  });
+  console.log('Connected to DB');
+});
