f1e30f42aa02882e4e93999bce9fec67e16d6ee7
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-24 17:10:32 +0100

    Add getHotelByHotelId controller function

--- src/controllers/hotelController.js
+++ src/controllers/hotelController.js
@@ -9,3 +9,22 @@ async function getHotels(request, response) {
       response.status(400).json({ error: error.message });
     }
   }
+
+
+  async function getHotelByHotelId(request, response) {
+    console.log(request.params);
+    const { hotelId } = request.params;
+  
+    try {
+      const hotel = await HotelDB.find({ _id: hotelId });
+  
+      response.status(200).json(hotel);
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+
+  module.exports = {
+    getHotels,
+    getHotelByHotelId
+  }
