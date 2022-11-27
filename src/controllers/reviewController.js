66014249f070c41b453c5cba9134778c267233ee
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-27 14:05:14 +0100

    Like/dislike functionallity

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,9 +1,12 @@
 const Review = require('../models/reviewModel');
 
 async function getReviewsByHotelId(request, response) {
-    const { hotelId } = request.body;
+    const { hotelId } = request.params;
     try {
-      const reviews = await Review.find({ hotelId });
+      const reviews = await Review.find({ hotelId }).populate({
+        path: 'createdBy',
+        select: ['_id', 'email', 'displayName', 'role'],
+      });
   
       response.status(200).json(reviews);
     } catch (error) {
@@ -30,8 +33,83 @@ async function getReviewsByHotelId(request, response) {
     }
   }
 
+  async function updateLike(request, response) {
+    const { reviewId } = request.body;
+  
+    const { likes } = await Review.findOne({
+      _id: reviewId,
+    });
+  
+    const update = {
+      $set: {
+        likes: likes?.find(
+          (id) => id.toString() === request.user[0]._id.toString()
+        )
+          ? likes.filter((id) => id.toString() !== request.user[0]._id.toString())
+          : [...likes, request.user[0]._id],
+      },
+    };
+  
+    try {
+      const updatedReview = await Review.findOneAndUpdate(
+        { _id: reviewId },
+        update,
+        {
+          returnOriginal: false,
+        }
+      ).populate({
+        path: 'createdBy',
+        select: ['_id', 'email', 'displayName', 'role'],
+      });
+      console.log(updatedReview);
+      response.status(200).json(updatedReview);
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+
+
+  async function updateDislike(request, response) {
+    const { reviewId } = request.body;
+  
+    const { dislikes } = await Review.findOne({
+      _id: reviewId,
+    });
+  
+    const update = {
+      $set: {
+        dislikes: dislikes?.find(
+          (id) => id.toString() === request.user[0]._id.toString()
+        )
+          ? dislikes.filter(
+              (id) => id.toString() !== request.user[0]._id.toString()
+            )
+          : [...dislikes, request.user[0]._id],
+      },
+    };
+  
+    try {
+      const updatedReview = await Review.findOneAndUpdate(
+        { _id: reviewId },
+        update,
+        {
+          returnOriginal: false,
+        }
+      ).populate({
+        path: 'createdBy',
+        select: ['_id', 'email', 'displayName', 'role'],
+      });
+  
+      response.status(200).json(updatedReview);
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+
 
   module.exports = {
     getReviewsByHotelId,
-    createReview
+    createReview,
+    updateLike,
+    updateDislike
   }
