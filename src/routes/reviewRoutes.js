980404b740b07b872be143cee2c231d95877c0cd
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-27 14:06:04 +0100

    Add updateDislike route

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const requireAuth = require('../middleware/requireAuth')
+const {getReviewsByHotelId, createReview, updateLike, updateDislike} = require('../controllers/reviewController')
 
-router.get('/', getReviewsByHotelId);
+
+router.get('/:hotelId', getReviewsByHotelId);
+
 router.use(requireAuth)
 router.post('/', createReview);
 router.put('/', updateLike);
+router.put('/dislike', updateDislike)
 
 module.exports = router;
