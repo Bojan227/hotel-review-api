50d21a65d0fe7cb2cb36dd79e2aee696db80a6d5
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-27 16:27:49 +0100

    import controllers for hotel routes

--- src/routes/hotelRoutes.js
+++ src/routes/hotelRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const requireAuth = require('../middleware/requireAuth')
+const {getHotels, getHotelByHotelId, createHotel, editHotelDetails} = require('../controllers/hotelController')
+
 
 router.get('/', getHotels);
 router.get('/:hotelId', getHotelByHotelId);
