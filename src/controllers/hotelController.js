4e13a41bd485fc222335fb127b0156cee400b017
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-29 14:27:13 +0100

    Add skip and limit options and count the number of hotels in the collection

--- src/controllers/hotelController.js
+++ src/controllers/hotelController.js
@@ -1,77 +1,81 @@
 const HotelDB = require('../models/hotelModel');
-const uploadImage = require('../utils/uploadImage')
+const uploadImage = require('../utils/uploadImage');
 
 async function getHotels(request, response) {
-  const { name, address } = request.query;
+  const { name, address, page } = request.query;
   try {
     const hotels = await HotelDB.find({
       hotelName: { $regex: name, $options: 'i' },
       address: { $regex: address, $options: 'i' },
     })
+      .skip(page * 3)
+      .limit(3)
       .collation({ locale: 'en', strength: 2 })
       .sort({ hotelName: 1 });
 
-    console.log(hotels);
-    response.status(200).json(hotels);
+    const countHotels = await HotelDB.aggregate([{ $count: 'hotelName' }]);
+
+    response.status(200).json({ hotels, countHotels });
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
 }
 
+async function getHotelByHotelId(request, response) {
+  const { hotelId } = request.params;
 
-  async function getHotelByHotelId(request, response) {
-    console.log(request.params);
-    const { hotelId } = request.params;
-  
-    try {
-      const hotel = await HotelDB.find({ _id: hotelId });
-  
-      response.status(200).json(hotel);
-    } catch (error) {
-      response.status(400).json({ error: error.message });
-    }
+  try {
+    const hotel = await HotelDB.find({ _id: hotelId });
+
+    response.status(200).json(hotel);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
   }
+}
 
-  async function createHotel(request, response) {
-    const { hotelName, address, text, image } = request.body;
-  
-    try {
-      const { imageUrl, imageId } = await uploadImage(image);
-  
-      const hotel = await HotelDB.create({
-        hotelName,
-        address,
-        imageId,
-        imageUrl,
-        text,
-      });
-  
-      response.status(200).json(hotel);
-    } catch (error) {
-      response.status(400).json({ error: error.message });
-    }
+async function createHotel(request, response) {
+  const { hotelName, address, text, image } = request.body;
+
+  try {
+    const { imageUrl, imageId } = await uploadImage(image);
+
+    const hotel = await HotelDB.create({
+      hotelName,
+      address,
+      imageId,
+      imageUrl,
+      text,
+    });
+
+    response.status(200).json(hotel);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
   }
+}
 
-  async function editHotelDetails(request, response) {
-    const { hotelName, address, text, hotelId } = request.body;
-  
-    const updateInfo = { hotelName, address, text };
-  
-    try {
-      const newHotel = await HotelDB.findOneAndUpdate({ _id: hotelId }, updateInfo, {
+async function editHotelDetails(request, response) {
+  const { hotelName, address, text, hotelId } = request.body;
+
+  const updateInfo = { hotelName, address, text };
+
+  try {
+    const newHotel = await HotelDB.findOneAndUpdate(
+      { _id: hotelId },
+      updateInfo,
+      {
         returnOriginal: false,
-      });
-  
-      response.status(200).json(newHotel);
-    } catch (error) {
-      response.status(400).json({ error: error.message });
-    }
-  }
+      }
+    );
 
-
-  module.exports = {
-    getHotels,
-    getHotelByHotelId,
-    createHotel,
-    editHotelDetails
+    response.status(200).json(newHotel);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
   }
+}
+
+module.exports = {
+  getHotels,
+  getHotelByHotelId,
+  createHotel,
+  editHotelDetails,
+};
